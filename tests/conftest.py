09cc28fca80a27070c68f7dc2bd31a9081be8cee
sladkovm <sladkovm@gmail.com>
2018-02-03 15:09:27 +0100

    fix conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,9 @@ from vmpy import strava
 @pytest.fixture
 def test_stream():
 
-    f_name = os.path.normpath('tests/assets/streams_1202065_1354978421.json')
+    _here = os.path.abspath(__file__)
+
+    f_name = os.path.normpath(os.path.join(_here, '../assets/streams_1202065_1354978421.json'))
     with open(f_name) as f:
 
         _stream = json.load(f)
